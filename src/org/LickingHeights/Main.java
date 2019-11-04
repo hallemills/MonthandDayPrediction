fdf9c63ed2e092d19e860699899c149b5afcef56
halle <halle.mills.2022@student.lhschools.org>
2019-11-04 09:54:54 -0500

    created the poem

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,37 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Monday's child is fair of face");
+        System.out.println("Tuesday's child if full of grace");
+        System.out.println("Wednesday's child is full of woe");
+        System.out.println("Thursday's child has far to go");
+        System.out.println("Friday's child is loving and giving");
+        System.out.println("Saturday's child works hard for living");
+        System.out.println("But the child born on Sabbath Day");
+        System.out.println("Is fair and wise and good in every way");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
