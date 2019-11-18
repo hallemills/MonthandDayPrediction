881aa48de870e7004ee3010aef826553d6550646
halle <halle.mills.2022@student.lhschools.org>
2019-11-18 09:23:49 -0500

    made a scanner

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,37 +1,57 @@
 package org.LickingHeights;
 
+import java.time.MonthDay;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
         System.out.println("Monday's child is fair of face");
-        System.out.println("Tuesday's child if full of grace");
+        System.out.println("Tuesday's child is full of grace");
         System.out.println("Wednesday's child is full of woe");
         System.out.println("Thursday's child has far to go");
         System.out.println("Friday's child is loving and giving");
-        System.out.println("Saturday's child works hard for living");
+        System.out.println("Saturday's child works hard for a living");
         System.out.println("But the child born on Sabbath Day");
         System.out.println("Is fair and wise and good in every way");
+
+
+        Scanner keyboard = new Scanner(System.in);
+        boolean loop = true
+        while (loop) ;
+        int month = keyboard.nextInt();
+        String phrase = 
+        System.out.println(phrase);
+
 
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
