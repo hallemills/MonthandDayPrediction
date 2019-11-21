2afcda55aaa3dc514de912e45b7903405e71e7d5
halle <halle.mills.2022@student.lhschools.org>
2019-11-21 09:45:30 -0500

    made my commit for the days of the month

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -44,7 +44,77 @@ public class Main {
         }
 
 
-        public static boolean 
+        public static boolean
+
+
+
+
+
+
+
+
+
+
+
+        public static int zellerCongruence(int month, int year, int dayOfmonth) {
+
+            if (month == 1 || month == 2) {
+                month = (month + 12);
+                year--;
+            }
+            int yearZeroBased;
+            yearZeroBased = (year / 100);
+            int zellersCongruence;
+            zellersCongruence = 13 * (month + 1);
+            int yearofCentury;
+            yearofCentury = (year % 100);
+
+
+
+
+            int dayofWeek;
+            dayofWeek = ((dayOfmonth + ((zellersCongruence) / 5) + yearOfcentury + (yearOfcentury / 4) + ((yearZeroBased) / 4) - (2 * (yearZeroBased))) % 7);
+            return dayofWeek;
+
+        }
+
+
+        public static String MonthPoem(int dayofWeek) {
+            if (dayofWeek == 1) {
+                return "You were born on Monday \n" +
+                        "According to the poem, that means you are fair of face";
+            } else if (dayofWeek == 2) {
+                return "You were born on Tuesday \n" +
+                        "According to the poem, you're the child that is full of grace";
+            } else if (dayofWeek == 3) {
+                return "You were born on Wednesday \n" +
+                        "According to the poem, you are full of woe";
+            } else if (dayofWeek == 4) {
+                return "You were born on Thursday \n" +
+                        "According to the poem, you have far to go";
+            } else if (dayofWeek == 5) {
+                return "You were born on Friday \n" +
+                        "According to the poem, you the child that is loving and giving";
+            } else if (dayofWeek == 6) {
+                return "You were born on Saturday \n" +
+                        "According to the poem, you work hard for a living";
+            } else  {
+                return "You were born on Sabbath day" +
+                        "According to the poem, you are fair and wise in every way";
+            }
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
