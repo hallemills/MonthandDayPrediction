6555f23dd89023b4a7f638c1925082f8d9400f73
halle <halle.mills.2022@student.lhschools.org>
2019-11-21 09:43:54 -0500

    made my switch

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -6,53 +6,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int dayOfweek;
+        int dayOfmonth;
+        int month;
+        int year;
+        int yearOfcentury;
+
+        int ZellerCongruence;
+        int answer;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
 
 
-        System.out.println("Monday's child is fair of face");
-        System.out.println("Tuesday's child is full of grace");
-        System.out.println("Wednesday's child is full of woe");
-        System.out.println("Thursday's child has far to go");
-        System.out.println("Friday's child is loving and giving");
-        System.out.println("Saturday's child works hard for a living");
-        System.out.println("But the child born on Sabbath Day");
-        System.out.println("Is fair and wise and good in every way");
+        if {
 
 
+            System.out.println("Monday's child is fair of face");
+            System.out.println("Tuesday's child is full of grace");
+            System.out.println("Wednesday's child is full of woe");
+            System.out.println("Thursday's child has far to go");
+            System.out.println("Friday's child is loving and giving");
+            System.out.println("Saturday's child works hard for a living");
+            System.out.println("But the child born on Sabbath Day");
+            System.out.println("Is fair and wise and good in every way");
+
+            System.out.println("What year were you born?");
+            year = keyboard.nextInt();
+            System.out.println("What month were you born, give me the number");
+            month = keyboard.nextInt();
+            System.out.println("What is the day you were born");
+            dayOfweek = keyboard.nextInt();
+            int answer =
 
 
-
-
-
-
-    }
-
-    public static int ZellerCongruence(int month, int year, int dayofWeek) {
-
-        if (month == 1 || month == 2) {
-            month= (month + 12);
-            year--;
+            int answer2 = ZellerCongurence(month, year, dayOfmonth);
+            System.out.println(answer2);
         }
 
 
-
-
-
-
-
-
-
-
-    }
-}
-
-
-
-
-
-
-
-
-
+        public static boolean 
 
 
 
